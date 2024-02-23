c524225ee87c490a45be8ce2943ef2e1419fdeba
John Pandis <ioanpand@outlook.com>
2024-02-23 17:37:27 +0200

    Improved CustomCamera.cs

---
+++ Assets/_Scripts/Editor/CustomCameraEditor.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CustomCamera)) ]
+public class CustomCameraEditor : Editor {
+
+    private const string buttonName = "Align camera with target object";
+    SerializedProperty cameraPivot;
+    SerializedProperty followTransform;
+
+    private void OnEnable() {
+        cameraPivot = serializedObject.FindProperty(nameof(cameraPivot));
+        followTransform = serializedObject.FindProperty(nameof(followTransform));
+    }
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        GUILayout.Space(10);
+        if (GUILayout.Button(buttonName)) {
+            Transform pivotTransform = (Transform)cameraPivot.objectReferenceValue;
+            Transform targetTransform = (Transform)followTransform.objectReferenceValue;
+            if (pivotTransform != null && targetTransform != null ) {
+                pivotTransform.position = targetTransform.position;
+                pivotTransform.eulerAngles = targetTransform.eulerAngles;
+            }
+            else {
+                Debug.LogError("FollowTransform and/or CameraPivot reference(s) have not been assigned.");
+            }
+        }
+
+    }
+}
