b5a501307a3aeeaf82ca6c1abd056bcb090d13dc
John Pandis <ioanpand@outlook.com>
2024-02-26 04:20:09 +0200

    Update AIGunner.cs
    The AI can now patrol an area based on the provided patrol points.

--- Assets/_Scripts/AIGunner.cs
+++ Assets/_Scripts/AIGunner.cs
@@ -1,31 +1,63 @@
+using CocaCopa.Utilities;
 using UnityEngine;
 
 public class AIGunner : AIController {
 
+    [SerializeField] private Transform[] patrolPoints;
+    [SerializeField] private float patrolTime;
+    private float patrolTimer;
+    private Vector3 moveDirection;
     private Transform targetTransform;
     private Vector3 targetMovePosition;
+    private int patrolPointIndex = 0;
 
     protected override void Awake() {
         base.Awake();
-        targetTransform = FindObjectOfType<PlayerController>().transform;
+        patrolTimer = patrolTime;
+    }
+
+    protected override void Start() {
+        base.Start();
+        State = AI_State.Patrol;
+        SetNewDestination(patrolPoints[0].position);
+        patrolPointIndex++;
     }
 
     protected override void Update() {
         base.Update();
-        SetLookAtObjectPosition(targetTransform.position + Vector3.up * 1.2f);
-        if (Stimulus.CanSeeTarget(targetTransform)) {
-            combatManager.ReleaseGunTrigger();
-            combatManager.PullGunTrigger();
-            if (combatManager.EquippedWeapon.RemainingBullets == 0) {
-                combatManager.SwitchToSecondary();
-            }
+        IsRunning = false;
+        StateManager();
+    }
 
-            targetMovePosition = FindHideSpot(targetTransform);
-            SetNewDestination(targetMovePosition);
+    private void StateManager() {
+        switch (State) {
+            case AI_State.Patrol:
+            StatePatrol();
+            break;
+            case AI_State.Investigate:
+            StateInvestigate();
+            break;
+            default:
+            break;
         }
     }
 
-    private void FixedUpdate() {
-        movement.MoveTowardsDirection(InputDirection, run: false);
+    private void StatePatrol() {
+        if (ReachedPathDestination && Common.TickTimer(ref patrolTimer, patrolTime)) {
+            if (patrolPointIndex >= patrolPoints.Length) {
+                patrolPointIndex = 0;
+            }
+
+            SetNewDestination(patrolPoints[patrolPointIndex].position);
+            patrolPointIndex++;
+        }
+        if (DirectionalInput != Vector2.zero)
+        moveDirection = new Vector3(DirectionalInput.x, 0f, DirectionalInput.y) * 2f;
+        Vector3 eyesLevel = Vector3.up;
+        SetLookAtObjectPosition(transform.position + moveDirection + eyesLevel);
+    }
+
+    private void StateInvestigate() {
+
     }
 }
